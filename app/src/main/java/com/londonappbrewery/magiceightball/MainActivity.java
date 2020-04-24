f5e74807ecebdd87cbb707b9602e154e70018487
Aman Verma <adidikitti@gmail.com>
2020-04-24 20:23:09 +0530

    Project Completed. Appearance in Window may not be very appealing. Run it on a physical device.

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -1,7 +1,13 @@
 package com.londonappbrewery.magiceightball;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final int[] Ballarr = { R.drawable.ball1,
+        R.drawable.ball2,
+        R.drawable.ball3,
+        R.drawable.ball4,
+        R.drawable.ball5};
+        Button Ask;
+        Ask = findViewById(R.id.AskButton);
+        final ImageView img;
+        img = findViewById(R.id.Ball);
+
+        Ask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random rn = new Random();
+                int n;
+                n = rn.nextInt(5);
+                img.setImageResource(Ballarr[n]);
+            }
+        });
+
     }
 }
